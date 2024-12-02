5fe3ee0ef5274b62bc5fba6370a33d5d8b79abab
unknown <marcel0paixao>
2024-12-01 21:20:17 -0300

    fixed third exercise

--- submission/003.sh
+++ submission/003.sh
@@ -1,23 +1,9 @@
 # How many new outputs were created by block 123,456?
 
-# Get the block hash for block number 123,456
-block_hash=$(bitcoin-cli getblockhash 123456)
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-# Retrieve the block details using the block hash
-block_details=$(bitcoin-cli getblock $block_hash 2)
+descriptor="tr($xpub/100)"
 
-# Initialize a counter for the outputs
-output_count=0
+taproot_address=$(bitcoin-cli -rpcuser=bitcoinuser -rpcpassword=bitcoinpassword getaddressinfo "$descriptor" | jq -r '.address')
 
-# Iterate over each transaction in the block
-for txid in $(echo $block_details | jq -r '.tx[]'); do
-  # For each transaction, get the number of outputs
-  tx_details=$(bitcoin-cli getrawtransaction $txid 1)
-  num_outputs=$(echo $tx_details | jq '.vout | length')
-  
-  # Add the number of outputs to the counter
-  output_count=$((output_count + num_outputs))
-done
-
-# Output the total number of new outputs
-echo $output_count
+echo "Taproot address at index 100: $taproot_address"
