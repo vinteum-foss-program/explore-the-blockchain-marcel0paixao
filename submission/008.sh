57f78ba33c83ef19bdf3f8783c4db14e4966b6a8
marcel0paixao <marceloht461@gmail.com>
2024-12-02 02:14:51 -0300

    fixed 8th with cut string

--- submission/008.sh
+++ submission/008.sh
@@ -5,6 +5,6 @@ txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
 raw_tx=$(bitcoin-cli getrawtransaction "$txid" 2)
 
-public_key=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[2]')
+public_key=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[2] | .[4:70]')
 
-echo "$public_key"
+echo "$public_key[4:68]"
