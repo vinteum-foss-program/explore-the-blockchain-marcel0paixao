3d0d7982a4a9458d75a9039f6372f557df5a277a
marcel0paixao <marceloht461@gmail.com>
2024-12-02 02:00:45 -0300

    done 8th

--- submission/008.sh
+++ submission/008.sh
@@ -1,3 +1,10 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
-6321025d524ac7ec6501d018d322334f142c7c11aa24b9cffec03161eca35a1e32a71f67029000b2752102ad92d02b7061f520ebb60e932f9743a43fee1db87d2feb1398bf037b3f119fc268ac
+
+txid="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+raw_tx=$(bitcoin-cli getrawtransaction "$txid" 2)
+
+public_key=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[2]')
+
+echo "$public_key"
