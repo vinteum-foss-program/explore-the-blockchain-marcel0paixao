49eb1a7915ee4e5b98b2cd75cccf96d7174596d8
unknown <marcel0paixao>
2024-12-01 21:35:57 -0300

    fixed third  exercise

--- submission/003.sh
+++ submission/003.sh
@@ -1,15 +1,15 @@
 # How many new outputs were created by block 123,456?
 
-block_hash=$(bitcoin-cli -rpcuser=bitcoinuser -rpcpassword=bitcoinpassword getblockhash 123456)
+#!/bin/bash
 
-block_details=$(bitcoin-cli -rpcuser=bitcoinuser -rpcpassword=bitcoinpassword getblock $block_hash 2)
-
+BLOCK_HEIGHT=123456
+block_hash=$(bitcoin-cli getblockhash $BLOCK_HEIGHT)
+block_details=$(bitcoin-cli getblock $block_hash 2)
 output_count=0
 
-for tx in $(echo "$block_details" | jq -r '.tx[]'); do
-  num_outputs=$(echo "$block_details" | jq -r ".tx[] | select(.txid == \"$tx\") | .vout | length")
-
+for txid in $(echo "$block_details" | jq -r '.tx[]'); do
+  num_outputs=$(echo "$block_details" | jq -r ".tx[] | select(.txid == \"$txid\") | .vout | length")
   output_count=$((output_count + num_outputs))
 done
 
-echo $output_count
+echo "$output_count"
