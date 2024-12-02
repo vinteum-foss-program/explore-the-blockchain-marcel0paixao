d7201106044d919200c92b88868852b9ac092445
marcel0paixao <celomar01@hotmail.com>
2024-12-01 23:21:01 -0300

    fixed fourth

--- submission/004.sh
+++ submission/004.sh
@@ -1,8 +1,12 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
-xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-index=100
-address=$(bitcoin-cli getdescriptorinfo "wpkh([${xpub}]${index}/*)#0f3ff5ab" | jq -r '.descriptor' | bitcoin-cli getaddressinfo | jq -r '.address')
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 
-echo "$address"
+DESCRIPTOR="tr([00000000/0'/0']$XPUB/100)"
+
+DESCRIPTOR_WITH_CHECKSUM=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+
+ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM" | jq -r '.[0]')
+
+echo "$ADDRESS"
