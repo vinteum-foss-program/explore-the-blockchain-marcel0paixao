e5decd048923b2b1919fa188c3e0056223c40974
marcel0paixao <marceloht461@gmail.com>
2024-12-02 01:29:07 -0300

    6th exercise

--- submission/006.sh
+++ submission/006.sh
@@ -1,2 +1,14 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
-c54714cb1373c2e3725261fe201f267280e21350bdf2df505da8483a6a4805fc
+
+block1=$(bitcoin-cli getblockhash 256128)
+coinbase_tx=$(bitcoin-cli getblock $block1 | jq -r '.tx[0]')
+block2=$(bitcoin-cli getblockhash 257343)
+transactions=$(bitcoin-cli getblock $block2 | jq -r '.tx[]')
+
+for tx in $transactions; do
+  inputs=$(bitcoin-cli getrawtransaction $tx 1 | jq -r '.vin[].txid')
+  if [[ $inputs == *"$coinbase_tx"* ]]; then
+    echo $tx
+    break
+  fi
+done
